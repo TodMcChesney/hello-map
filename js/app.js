e54182a951a87a24384f46dfb8c31f8947829d14
TodMcChesney <codescribe.net@gmail.com>
2017-05-01 09:52:31 +0200

    refactor: Move JS from index.html to js/app.js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// TODO: Create a map variable
+// TODO: Complete the following function to initialize the map
+function initMap() {
+    // TODO: use a constructor to create a new map JS object. You can use the coordinates we used, 40.7413549, -73.99802439999996 or your own!
+}
